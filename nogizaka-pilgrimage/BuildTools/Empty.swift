e1b845221c1213f79b5c90e21a2b585631fe5e5d
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-12-27 18:41:59 +0900

    [add]R.generatedをgitの追跡対象から除外

---
+++ nogizaka-pilgrimage/BuildTools/Empty.swift
@@ -0,0 +1,8 @@
+//
+//  Empty.swift
+//  nogizaka-pilgrimage
+//
+//  Created by 工藤 海斗 on 2022/12/27.
+//
+
+import Foundation
---
+++ nogizaka-pilgrimage/BuildTools/Package.swift
@@ -0,0 +1,8 @@
+//
+//  Package.swift
+//  nogizaka-pilgrimage
+//
+//  Created by 工藤 海斗 on 2022/12/27.
+//
+
+import Foundation
