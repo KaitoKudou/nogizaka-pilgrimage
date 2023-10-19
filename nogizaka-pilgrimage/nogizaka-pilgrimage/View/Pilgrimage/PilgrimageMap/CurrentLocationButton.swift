ae5b57e19d1fd8ee1fd04ba3cca55e14aa70ad9e
Kaito Kudou <kaitokudou2468110921@gmail.com>
2023-10-19 12:16:39 +0900

    [add]現在地ボタンのUIを作成

---
+++ nogizaka-pilgrimage/nogizaka-pilgrimage/View/Pilgrimage/PilgrimageMap/CurrentLocationButton.swift
@@ -0,0 +1,44 @@
+//
+//  CurrentLocationButton.swift
+//  nogizaka-pilgrimage
+//
+//  Created by 工藤 海斗 on 2023/10/18.
+//
+
+import SwiftUI
+
+/// 現在地ボタン
+struct CurrentLocationButton: View {
+    @Environment(\.theme) private var theme
+    let didTap: () -> Void
+
+    var body: some View {
+        Button {
+            // TODO: ボタン押下時に現在地を表示
+            print("現在地ボタン押下")
+            didTap()
+        } label: {
+            Image(systemName: "location")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.all, theme.margins.spacing_xs)
+                .foregroundStyle(.gray)
+                .background(.white)
+                .clipShape(Circle())
+                .frame(
+                    width: theme.margins.spacing_xxl,
+                    height: theme.margins.spacing_xxl
+                )
+                .padding(.all, theme.margins.spacing_m)
+                .shadow(
+                    color: .black.opacity(0.25),
+                    radius: 4.0, x: .zero, y: 1.0
+                )
+        }
+    }
+}
+
+#Preview {
+    CurrentLocationButton(didTap: {})
+        .previewLayout(.sizeThatFits)
+}
