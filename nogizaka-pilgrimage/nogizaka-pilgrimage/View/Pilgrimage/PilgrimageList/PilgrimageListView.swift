08dff9d13900ba40fdb9709c4dacb1e9522fa7c0
Kaito Kudou <kaitokudou2468110921@gmail.com>
2023-10-25 00:20:18 +0900

    [add]一覧画面を作成

---
+++ nogizaka-pilgrimage/nogizaka-pilgrimage/View/Pilgrimage/PilgrimageList/PilgrimageListView.swift
@@ -0,0 +1,62 @@
+//
+//  PilgrimageListView.swift
+//  nogizaka-pilgrimage
+//
+//  Created by 工藤 海斗 on 2023/10/19.
+//
+
+import SwiftUI
+
+struct PilgrimageListView: View {
+    @Environment(\.theme) private var theme
+    @State private var pilgrimageListType: PilgrimageListType = .all
+    @State private var searchWord = ""
+
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                searchTextField
+                    .background(.white)
+                    .border(.gray, width: 1)
+                    .cornerRadius(8.0)
+                    .padding(.top, theme.margins.spacing_xxs)
+                    .padding(.bottom, theme.margins.spacing_m)
+                    .padding(.horizontal, theme.margins.spacing_m)
+                    .background(R.color.tab_primary()!.color)
+
+                PilgrimageListTypeSelectView(
+                    type: $pilgrimageListType,
+                    screenWidth: geometry.size.width
+                )
+            }
+        }
+        .navigationTitle(R.string.localizable.navbar_pilgrimage_list())
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonTextHidden()
+    }
+
+    private var searchTextField: some View {
+        HStack(spacing: .zero) {
+            Image(systemName: "magnifyingglass")
+                .foregroundStyle(.gray)
+                .padding(.leading, theme.margins.spacing_xs)
+
+            TextField(
+                R.string.localizable.pilgrimage_list_placeholder(),
+                text: $searchWord
+            )
+            .padding(.vertical, theme.margins.spacing_xs)
+            .padding(.leading, theme.margins.spacing_xxs)
+            .padding(.trailing, theme.margins.spacing_m)
+            .submitLabel(.search)
+            .onSubmit {
+                // TODO: 検索
+                print("検索予定ワード: \(searchWord)")
+            }
+        }
+    }
+}
+
+#Preview {
+    PilgrimageListView()
+}
