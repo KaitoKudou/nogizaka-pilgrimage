146972109550bacc902f9b5de5f662a20162f17e
Kaito Kudou <kaitokudou2468110921@gmail.com>
2023-11-06 23:41:46 +0900

    [fix]お気に入り/一覧を選択するバーの見た目を修正

--- nogizaka-pilgrimage/nogizaka-pilgrimage/View/Pilgrimage/PilgrimageList/PilgrimageListTypeSelectView.swift
+++ nogizaka-pilgrimage/nogizaka-pilgrimage/View/Pilgrimage/PilgrimageList/PilgrimageListTypeSelectView.swift
@@ -15,7 +15,7 @@ struct PilgrimageListTypeSelectView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: .zero) {
-            HStack(spacing: screenWidth / 3) {
+            HStack {
                 ForEach(PilgrimageListType.allCases, id: \.self) { type in
                     Button {
                         self.type = type
@@ -29,9 +29,9 @@ struct PilgrimageListTypeSelectView: View {
                             )
                             .font(theme.fonts.bodyLarge)
                     }
+                    .frame(maxWidth: .infinity, maxHeight: screenWidth / 10)
                 }
             }
-            .padding(.horizontal, theme.margins.spacing_xxl)
 
             Rectangle()
                 .fill(R.color.text_secondary()!.color)
@@ -45,7 +45,7 @@ struct PilgrimageListTypeSelectView: View {
 
     /// - Returns: ボタンの横幅
     private func buttonWidth() -> CGFloat {
-        let width = (screenWidth - theme.margins.spacing_xxl * 2) / 2
+        let width = screenWidth / 2
         return max(0, width)
     }
 
@@ -54,10 +54,10 @@ struct PilgrimageListTypeSelectView: View {
     private func barOffset() -> CGFloat {
         switch type {
         case .all:
-            return theme.margins.spacing_xs
+            return 0
 
         case .favorite:
-            return screenWidth / 2 + (theme.margins.spacing_xs + theme.margins.spacing_l)
+            return screenWidth / 2
         }
     }
 }
