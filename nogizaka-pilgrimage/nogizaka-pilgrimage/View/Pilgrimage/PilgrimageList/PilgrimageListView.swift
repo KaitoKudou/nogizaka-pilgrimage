e2f0228abee3520488d3dcd5323b31aad2dc375f
Kaito Kudou <kaitokudou2468110921@gmail.com>
2023-12-06 23:30:00 +0900

    [fix]一覧画面からお気に入りを剥がした

--- nogizaka-pilgrimage/nogizaka-pilgrimage/View/Pilgrimage/PilgrimageList/PilgrimageListView.swift
+++ nogizaka-pilgrimage/nogizaka-pilgrimage/View/Pilgrimage/PilgrimageList/PilgrimageListView.swift
@@ -10,42 +10,31 @@ import SwiftUI
 
 struct PilgrimageListView: View {
     @Environment(\.theme) private var theme
-    @State private var pilgrimageListType: PilgrimageListType = .all
     @State private var searchWord = ""
-    @State var store = Store(initialState: FavoriteFeature.State()) {
-        FavoriteFeature()
+    @State var store = Store(initialState: SearchCandidateFeature.State()) {
+        SearchCandidateFeature()
     }
 
     var body: some View {
         WithViewStore(store, observe: { $0 }) { viewStore in
             GeometryReader { geometry in
                 VStack {
-                    searchTextField
+                    searchTextField(viewStore: viewStore)
                         .padding(.top, theme.margins.spacing_xxs)
                         .padding(.bottom, theme.margins.spacing_m)
                         .padding(.horizontal, theme.margins.spacing_m)
                         .background(R.color.tab_primary()!.color)
 
-                    PilgrimageListTypeSelectView(
-                        type: $pilgrimageListType,
-                        screenWidth: geometry.size.width
+                    PilgrimageListNavigationView(
+                        pilgrimageList: viewStore.allSearchCandidatePilgrimages
                     )
-
-                    switch(pilgrimageListType) {
-                    case .all:
-                        pilgrimageListNavigationView(
-                            geometry: geometry,
-                            pilgrimageList: dummyPilgrimageList
-                        )
-                    case.favorite:
-                        pilgrimageListNavigationView(
-                            geometry: geometry,
-                            pilgrimageList: viewStore.state.favoritePilgrimages
-                        )
-                    }
                 }
-                .onChange(of: viewStore.state.favoritePilgrimages) { _ in
-                    viewStore.send(.fetchFavorites)
+                .onAppear {
+                    viewStore.send(.resetPilgrimages)
+                }
+                .onChange(of: viewStore.isLoading) { newIsLoading in
+                    if newIsLoading {
+                    }
                 }
             }
         }
@@ -54,33 +43,9 @@ struct PilgrimageListView: View {
         .navigationBarBackButtonTextHidden()
     }
 
-    private func pilgrimageListNavigationView(geometry: GeometryProxy, pilgrimageList: [PilgrimageInformation]) -> some View {
-        NavigationView {
-            List {
-                ForEach(pilgrimageList, id: \.code) { pilgrimage in
-                    ZStack {
-                        NavigationLink(
-                            destination: PilgrimageDetailView(
-                                pilgrimage: pilgrimage,
-                                store: store
-                            )
-                        ) {
-                            EmptyView()
-                        }
-                        .opacity(0)
-
-                        PilgrimageListContentView(pilgrimage: pilgrimage, store: store)
-                            .frame(maxHeight: geometry.size.width / 3)
-                    }
-                }
-                .listRowSeparator(.hidden)
-            }
-            .listStyle(.plain)
-        }
-        .navigationViewStyle(StackNavigationViewStyle())
-    }
-
-    private var searchTextField: some View {
+    private func searchTextField(
+        viewStore: ViewStore<SearchCandidateFeature.State, SearchCandidateFeature.Action>
+    ) -> some View {
         HStack(spacing: .zero) {
             Image(systemName: "magnifyingglass")
                 .foregroundStyle(.gray)
@@ -95,8 +60,12 @@ struct PilgrimageListView: View {
             .padding(.trailing, theme.margins.spacing_m)
             .submitLabel(.search)
             .onSubmit {
-                // TODO: 検索
-                print("検索予定ワード: \(searchWord)")
+                // キーボードの検索ボタンが押されたときにアクションを送信
+                if !searchWord.isEmpty {
+                    viewStore.send(.searchPilgrimages(searchWord))
+                } else {
+                    viewStore.send(.resetPilgrimages)
+                }
             }
         }
         .background(.white)
