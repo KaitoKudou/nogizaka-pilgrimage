43ed958ff9beb949e660167f8070240cda840102
Kaito Kudou <kaitokudou2468110921@gmail.com>
2024-03-30 00:22:16 +0900

    [add]AdMobのセットアップ

--- nogizaka-pilgrimage/nogizaka-pilgrimage/nogizaka_pilgrimageApp.swift
+++ nogizaka-pilgrimage/nogizaka-pilgrimage/nogizaka_pilgrimageApp.swift
@@ -6,11 +6,13 @@
 //
 
 import FirebaseCore
+import GoogleMobileAds
 import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
         FirebaseApp.configure()
         return true
     }
