d73ba156c5420b9410a0c214c0c9f319e9f15161
Kaito Kudou <kaitokudou2468110921@gmail.com>
2024-04-18 00:14:09 +0900

    [fix]お気に入りのstoreを修正

--- nogizaka-pilgrimage/nogizaka-pilgrimage/Feature/FavoriteFeature.swift
+++ nogizaka-pilgrimage/nogizaka-pilgrimage/Feature/FavoriteFeature.swift
@@ -12,21 +12,20 @@ import FirebaseFirestore
 struct FavoriteFeature {
     @ObservableState
     struct State: Equatable {
-        var favoritePilgrimages: [PilgrimageInformation] = []
+        var favoritePilgrimageRows = IdentifiedArrayOf<PilgrimageRowFeature.State>()
         var isLoading = false
-        var hasNetworkError = false
-        @Presents var alert: AlertState<Action>?
+        var favorited = false
+        @Presents var destination: Destination.State?
     }
 
-    enum Action: Equatable {
+    enum Action {
+        case onAppear
         case fetchFavorites
-        case updateFavoriteList(PilgrimageInformation)
-        case updateFavoriteCodes(PilgrimageInformation)
-        case alertDismissed(PresentationAction<Action>)
         case pilgrimageResponse(Result<[PilgrimageInformation], APIError>)
-        case startLoading
-        case stopLoading
-        case setNetworkError(Bool)
+        case setLoading(Bool)
+        case setFavorited(Bool)
+        case favoritePilgrimageRows(IdentifiedActionOf<PilgrimageRowFeature>)
+        case destination(PresentationAction<Destination.Action>)
     }
 
     @Dependency(\.networkMonitor) var networkMonitor
@@ -34,9 +33,11 @@ struct FavoriteFeature {
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
+            case .onAppear:
+                return .send(.fetchFavorites)
             case .fetchFavorites:
                 return .run { send in
-                    await send(.startLoading)
+                    await send(.setLoading(true))
 
                     do {
                         try await networkMonitor.monitorNetwork()
@@ -55,28 +56,29 @@ struct FavoriteFeature {
                         }
                         await send(.pilgrimageResponse(.success(favoritePilgrimages)))
                     } catch let error as NSError where error.domain == FirestoreErrorDomain {
-                        await send(.pilgrimageResponse(.failure(.fetchPilgrimagesError)))
-                    } catch {
-                        await send(.setNetworkError(true))
+                        await send(.pilgrimageResponse(.failure(.fetchFavoritePilgrimagesError)))
+                    } catch let error as APIError where error == APIError.networkError {
+                        await send(.pilgrimageResponse(.failure(.networkError)))
                     }
 
-                    await send(.stopLoading)
+                    await send(.setLoading(false))
                 }
-            case let .updateFavoriteList(pilgrimage):
+            case let .favoritePilgrimageRows(.element(id, .delegate(.favoriteButtonTapped(pilgrimage)))):
                 let uuid = UIDevice.current.identifierForVendor!.uuidString
                 let querySnapshot = Firestore.firestore().collection("favorite-list").document(uuid).collection("list")
                 let documentReference = querySnapshot.document(pilgrimage.name)
 
                 // ドキュメントを取得し、既にデータがある場合は削除し、ない場合はお気に入りに追加する
                 return .run { send in
-                    await send(.startLoading)
+                    await send(.favoritePilgrimageRows(.element(id: id, action: .setLoading(true))))
+
                     do {
                         try await networkMonitor.monitorNetwork()
-                        
+
                         if (try await querySnapshot.getDocuments().documents.first(where: { $0.documentID == pilgrimage.name })) != nil {
                             // 既にデータがある場合はお気に入りから削除
                             try await documentReference.delete()
-                            await send(.updateFavoriteCodes(pilgrimage))
+                            await send(.favoritePilgrimageRows(.element(id: id, action: .setFavorited(false))))
                         } else {
                             // ない場合はお気に入りに追加
                             let data: [String: Any?] = [
@@ -91,41 +93,69 @@ struct FavoriteFeature {
                                 "search_candidate_list": pilgrimage.searchCandidateList
                             ]
                             try await documentReference.setData(data as [String : Any])
-                            await send(.updateFavoriteCodes(pilgrimage))
+                            await send(.favoritePilgrimageRows(.element(id: id, action: .setFavorited(true))))
                         }
                         await send(.fetchFavorites)
                     } catch let error as NSError where error.domain == FirestoreErrorDomain {
-                        await send(.pilgrimageResponse(.failure(.fetchPilgrimagesError)))
-                    } catch {
-                        await send(.setNetworkError(true))
+                        await send(.favoritePilgrimageRows(.element(id: id, action: .pilgrimageResponse(.failure(.updateFavoritePilgrimagesError)))))
+                    } catch let error as APIError where error == APIError.networkError {
+                        await send(.favoritePilgrimageRows(.element(id: id, action: .pilgrimageResponse(.failure(.networkError)))))
                     }
-                    await send(.stopLoading)
+
+                    await send(.favoritePilgrimageRows(.element(id: id, action: .setLoading(false))))
                 }
-            case let .updateFavoriteCodes(pilgrimages):
-                UserDefaultsManager.shared.updateList(code: pilgrimages.code, userDefaultsKey: .favorite)
-                return .none
-            case .alertDismissed:
-                state.alert = nil
-                return .none
             case let .pilgrimageResponse(.success(pilgrimages)):
-                state.hasNetworkError = false
-                state.favoritePilgrimages = pilgrimages
+                state.favoritePilgrimageRows = .init(response: pilgrimages)
                 return .none
             case let .pilgrimageResponse(.failure(error)):
-                state.alert = .init(
-                    title: .init(error.localizedDescription)
-                )
+                switch error {
+                case .networkError:
+                    state.destination = .alert(.networkError)
+                case .fetchFavoritePilgrimagesError:
+                    state.destination = .alert(.fetchFavoritePilgrimagesError)
+                case .updateFavoritePilgrimagesError:
+                    state.destination = .alert(.updateFavoritePilgrimagesError)
+                default: return .none
+                }
                 return .none
-            case .startLoading:
-                state.isLoading = true
+            case let .setLoading(isLoading):
+                state.isLoading = isLoading
                 return .none
-            case .stopLoading:
-                state.isLoading = false
+            case let .setFavorited(favorited):
+                state.favorited = favorited
                 return .none
-            case let .setNetworkError(hasError):
-                state.hasNetworkError = hasError
+            case .favoritePilgrimageRows:
+                return .none
+            case .destination:
                 return .none
             }
         }
+        .forEach(\.favoritePilgrimageRows, action: \.favoritePilgrimageRows) {
+            PilgrimageRowFeature()
+        }
+        .ifLet(\.$destination, action: \.destination)
+    }
+}
+
+extension AlertState where Action == FavoriteFeature.Destination.Alert {
+    static let fetchFavoritePilgrimagesError = Self {
+        TextState(APIError.fetchFavoritePilgrimagesError.localizedDescription)
+    }
+
+    static let updateFavoritePilgrimagesError = Self {
+        TextState(APIError.updateFavoritePilgrimagesError.localizedDescription)
+    }
+
+    static let networkError = Self {
+        TextState(APIError.networkError.localizedDescription)
+    }
+}
+
+extension FavoriteFeature {
+    @Reducer(state: .equatable)
+    enum Destination {
+        case alert(AlertState<Alert>)
+
+        enum Alert: Equatable {}
     }
 }
