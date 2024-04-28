9fbcd744a08e0b6f156e00a25874aa8514f87ab7
Kaito Kudou <kaitokudou2468110921@gmail.com>
2024-04-29 00:05:55 +0900

    [fix]要素分が広告にならないように修正

--- nogizaka-pilgrimage/nogizaka-pilgrimage/View/FavoritePilgrimage/FavoritePilgrimageView.swift
+++ nogizaka-pilgrimage/nogizaka-pilgrimage/View/FavoritePilgrimage/FavoritePilgrimageView.swift
@@ -32,30 +32,31 @@ struct FavoritePilgrimageView: View {
                                     state: \.favoritePilgrimageRows,
                                     action: \.favoritePilgrimageRows
                                 )) { itemStore in
+                                    
                                     if itemStore.id % 5 == 0 {
                                         NativeAdvanceView()
                                             .frame(height: geometry.size.width / 3)
-                                    } else {
-                                        ZStack {
-                                            NavigationLink(
-                                                destination: PilgrimageDetailView(pilgrimage: itemStore.pilgrimage)
-                                            ) {
-                                                EmptyView()
-                                            }
-                                            .opacity(0)
-
-                                            PilgrimageListContentView(
-                                                pilgrimage: itemStore.pilgrimage, 
-                                                store: itemStore
-                                            )
-                                            .frame(maxHeight: geometry.size.width / 3)
-                                        }
-                                        .listRowSeparator(.hidden)
                                     }
+
+                                    ZStack {
+                                        NavigationLink(
+                                            destination: PilgrimageDetailView(pilgrimage: itemStore.pilgrimage)
+                                        ) {
+                                            EmptyView()
+                                        }
+                                        .opacity(0)
+
+                                        PilgrimageListContentView(
+                                            pilgrimage: itemStore.pilgrimage,
+                                            store: itemStore
+                                        )
+                                        .frame(maxHeight: geometry.size.width / 3)
+                                    }
+                                    .listRowSeparator(.hidden)
                                 }
                             }
-                            .listStyle(.plain)
                         }
+                        .listStyle(.plain)
                     }
                 }
             }
